85bc7c96dd51c125e09ff5169533c07706034df0
성채 조 <whtjdco1234@naver.com>
2024-05-06 23:32:07 +0900

    feat: Hang 추가, 조건에 맞도록 애니메이션 수정, 기타 오류 수정

--- Assets/Script/Players/NewController.cs
+++ Assets/Script/Players/NewController.cs
@@ -1,6 +1,10 @@
+using Spine.Unity;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class NewController : MonoBehaviour
 {
@@ -8,7 +12,7 @@ public class NewController : MonoBehaviour
     private Rigidbody2D rigid;
 
     [SerializeField]
-    private float speed;
+    private float speed = 4f;
     [SerializeField]
     private float walkSpeed = 4f;
     [SerializeField]
@@ -21,17 +25,28 @@ public class NewController : MonoBehaviour
     private float wallJmpForce = 400;
     [SerializeField]
     private int jumpCount = 0;
-    private float isRight = 1;
+    [SerializeField]
+    private float isRight = -1;
     [SerializeField]
     bool isWalk = false;
     [SerializeField]
     bool isRun = false;
+    [SerializeField]
     bool isDash = false;
     [SerializeField]
     bool isJump = false;
+    [SerializeField]
     bool isDrop = false;
     bool isHang = false;
     bool isWallJmp = false;
+    [SerializeField]
+    bool isWall = false;
+
+    private float wallChkDis = 0.15f;
+    public Transform wallChk;
+    public LayerMask w_Layer;
+    public SkeletonAnimation skeletonAnimation;
+    public AnimationReferenceAsset[] AnimClip;
 
     List<KeyCode> keys = new List<KeyCode>() { KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.Space, KeyCode.X, KeyCode.LeftShift};
 
@@ -47,6 +62,7 @@ public class NewController : MonoBehaviour
     }
 
     public State playerState;
+    private string currentState;
 
     private void Start()
     {
@@ -70,48 +86,285 @@ public class NewController : MonoBehaviour
             case State.WALK:
                 if (!isWalk)
                 {
-                    stateChange(State.IDLE);
+                    setCurrentState(State.IDLE);
                 }
                 break;
             case State.RUN:
                 if (!isRun)
                 {
-                    stateChange(State.IDLE);
+                    setCurrentState(State.IDLE);
                 }
                 break;
             case State.DASH:
                 if (!isDash)
                 {
-                    stateChange(State.IDLE);
+                    if (isJump)
+                    {
+                        setCurrentState(State.JUMP);
+                    }
+                    else if (isDrop)
+                    {
+                        setCurrentState(State.DROP);
+                    }
+                    else
+                        setCurrentState(State.IDLE);
                 }
                 break;
             case State.DROP:
                 if (!isDrop)
                 {
-                    stateChange(State.IDLE);
+                    setCurrentState(State.IDLE);
                 }
                 break;
             case State.JUMP:
                 if (!isJump)
                 {
-                    stateChange(State.IDLE);
+                    setCurrentState(State.DROP);
                 }
                 break;
             case State.HANG:
                 if (!isHang)
                 {
-                    stateChange(State.IDLE);
+                    setCurrentState(State.IDLE);
                 }
                 break;
         }
+
         if (rigid.velocity.y < 0)
         {
             isJump = false;
             isDrop = true;
         }
+
+        isWall = Physics2D.Raycast(wallChk.position, Vector2.right * isRight, wallChkDis, w_Layer);
         isWalk = false;
+
+        if (isWall)
+        {
+            isWallJmp = false;
+            rigid.velocity = new Vector2(rigid.velocity.x, 0);
+            setCurrentState(State.HANG);
+        }
     }
 
+    private void AscnAnimation(AnimationReferenceAsset animClip, bool loop, float timeScale)
+    {
+        if (animClip.name.Equals(currentState))
+            return;
+
+        skeletonAnimation.state.SetAnimation(0, animClip, loop).TimeScale = timeScale;
+        skeletonAnimation.loop = loop;
+        skeletonAnimation.timeScale = timeScale;
+
+        currentState = animClip.name;
+    }
+
+    private void setCurrentState(State state)
+    {
+        switch (state)
+        {
+            case State.IDLE:
+                rigid.velocity = Vector3.zero;
+                playerState = State.IDLE;
+                AscnAnimation(AnimClip[(int)state], true, 1f);
+                break;
+            case State.WALK:
+                playerState = State.WALK;
+                AscnAnimation(AnimClip[(int)state], true, 1.5f);
+                break;
+            case State.RUN:
+                playerState = State.RUN;
+                AscnAnimation(AnimClip[(int)state], true, 2f);
+                break;
+            case State.DASH:
+                isDash = true;
+                playerState = State.DASH;
+                AscnAnimation(AnimClip[(int)state], true, 3f);
+                break;
+            case State.JUMP:
+                playerState = State.JUMP;
+                AscnAnimation(AnimClip[(int)state], false, 1f);
+                break;
+            case State.DROP:
+                playerState = State.DROP;
+                AscnAnimation(AnimClip[(int)state], false, 1f);
+                break;
+            case State.HANG:
+                isHang = true;
+                playerState = State.HANG;
+                AscnAnimation(AnimClip[(int)state], true, 1f);
+                break;
+        }
+    }
+    void stopCounter()
+    {
+        isDash = false;
+        rigid.velocity = new Vector2(0, rigid.velocity.y);
+    }
+    void OnKeyboard(KeyCode key)
+    {
+        if (keys.Contains(key))
+        {
+            
+
+            if(key == KeyCode.LeftShift)
+            {
+                isRun = !isRun;
+            }
+
+            if(playerState != State.JUMP)
+            {
+                if (key == KeyCode.Space)
+                {
+                    isJump = true;
+                }
+                else if (isHang)
+                    return;
+            }
+
+            if (playerState != State.DASH)
+            {
+                if (key == KeyCode.X)
+                {
+                    isDash = true;
+                    Invoke("stopCounter", 0.2f);
+                }
+                else if (key != KeyCode.X)
+                {
+                    isWalk = true;
+                }
+            }
+            Move(key);
+        }
+    }
+
+    void Move(KeyCode key)
+    {
+        if (isWallJmp)
+            return;
+        if (isJump)
+        {
+            setCurrentState(State.JUMP);
+
+            if (isDash)
+            {
+                if (key != KeyCode.None)
+                    return;
+                speed = dashSpeed;
+                setCurrentState(State.DASH);
+            }
+            else if (isRun)
+            {
+                speed = runSpeed;
+            }
+            else if (isWalk)
+            {
+                speed = walkSpeed;
+            }
+        }
+        else if (isDrop)
+        {
+            setCurrentState(State.DROP);
+
+            if (isDash)
+            {
+                if (key != KeyCode.None)
+                    return;
+                speed = dashSpeed;
+                setCurrentState(State.DASH);
+            }
+            else if (isRun)
+            {
+                speed = runSpeed;
+            }
+            else if (isWalk)
+            {
+                speed = walkSpeed;
+            }
+
+        }
+        else
+        {
+            if(rigid.velocity.x == 0)
+            {
+                setCurrentState(State.IDLE);
+            }
+            else if (isDash)
+            {
+                speed = dashSpeed;
+                setCurrentState(State.DASH);
+            }
+            else if (isRun)
+            {
+                speed = runSpeed;
+                setCurrentState(State.RUN);
+            }
+            else if (isWalk)
+            {
+                speed = walkSpeed;
+                setCurrentState(State.WALK);
+            }
+        }
+
+        if (key == KeyCode.Space)
+        {
+            if (isHang)
+            {
+                isWallJmp = true;
+                Invoke("FreezeX", 0.3f);
+                rigid.velocity = new Vector2(-isRight * wallJmpForce, 0.9f * wallJmpForce);
+                Flip();
+            }
+            if (jumpCount < 2)
+            {
+                jumpCount++;
+                rigid.velocity = new Vector2(rigid.velocity.x, 0);
+                rigid.AddForce(new Vector2(0, jumpForce));
+            }
+        }
+
+        if (key == KeyCode.LeftArrow)
+        {
+            if (isRight == 1)
+                Flip();
+            rigid.velocity = new Vector2(speed * -1, rigid.velocity.y);
+        }
+        else if (key == KeyCode.RightArrow)
+        {
+            if (isRight != 1)
+                Flip();
+            rigid.velocity = new Vector2(speed, rigid.velocity.y);
+        }
+    }
+
+    private void Flip()
+    {
+        transform.eulerAngles = new Vector3(0, Mathf.Abs(transform.eulerAngles.y - 180), 0);
+        isRight = isRight * -1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isDrop = false;
+            jumpCount = 0;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(wallChk.position, Vector2.right * isRight * wallChkDis);
+    }
+
+    private void FreezeX()
+    {
+        isWallJmp = false;
+        isHang = false;
+    }
+
+    /*
     private void stateChange(State state)
     {
         switch(state)
@@ -154,144 +407,5 @@ public class NewController : MonoBehaviour
                 break;
         }
     }
-
-    void stopCounter()
-    {
-        isDash = false;
-    }
-    void OnKeyboard(KeyCode key)
-    {
-        if (keys.Contains(key))
-        {
-            if(key == KeyCode.LeftShift)
-            {
-                isRun = !isRun;
-            }
-
-            if(playerState != State.JUMP)
-            {
-                if (key == KeyCode.Space)
-                {
-                    isJump = true;
-                }
-            }
-
-            if (playerState != State.DASH)
-            {
-                if (key == KeyCode.X)
-                {
-                    stateChange(State.DASH);
-                }
-                else if (key != KeyCode.X)
-                {
-                    isWalk = true;
-                }
-            }
-            Move(key);
-
-            /*
-            if (key == KeyCode.LeftArrow || key == KeyCode.RightArrow)
-            {
-                isWalk = true;
-            }
-
-            if (key == KeyCode.Space)
-            {
-                stateChange(State.JUMP);
-            }
-            else if(rigid.velocity.y < 0)
-            {
-                stateChange(State.DROP);
-            }
-
-            if (key == KeyCode.X)
-            {
-                stateChange(State.DASH);
-            }
-            */
-        }
-    }
-
-    void Move(KeyCode key)
-    {
-        if (isJump && isDash)
-        {
-            speed = dashSpeed;
-            stateChange(State.DASH);
-        }
-        else if (isJump && isRun)
-        {
-            speed = runSpeed;
-            stateChange(State.JUMP);
-        }
-        else if(isJump && isWalk)
-        {
-            speed = walkSpeed;
-            stateChange(State.JUMP);
-        }
-        else if (isJump)
-        {
-            stateChange(State.JUMP);
-        }
-        else if (playerState == State.DASH)
-        {
-            speed = dashSpeed;
-        }
-        else if (isRun)
-        {
-            stateChange(State.RUN);
-        }
-        else if (isWalk)
-        {
-            stateChange(State.WALK);
-        }
-
-        if(isDrop && isDash)
-        {
-            speed = dashSpeed;
-            stateChange(State.DASH);
-        }
-        else if (isDrop)
-        {
-            stateChange(State.DROP);
-        }
-
-        if (key == KeyCode.Space)
-        {
-            if (jumpCount < 2)
-            {
-                jumpCount++;
-                rigid.velocity = new Vector2(rigid.velocity.x, 0);
-                rigid.AddForce(new Vector2(0, jumpForce));
-            }
-        }
-
-        if (key == KeyCode.LeftArrow)
-        {
-            if (isRight != 1)
-                Flip();
-            rigid.velocity = new Vector2(speed * -1, rigid.velocity.y);
-        }
-        else if (key == KeyCode.RightArrow)
-        {
-            if (isRight == 1)
-                Flip();
-            rigid.velocity = new Vector2(speed, rigid.velocity.y);
-        }
-    }
-
-    private void Flip()
-    {
-        transform.eulerAngles = new Vector3(0, Mathf.Abs(transform.eulerAngles.y - 180), 0);
-        isRight = isRight * -1;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.contacts[0].normal.y > 0.7f)
-        {
-            isDrop = false;
-            jumpCount = 0;
-        }
-    }
+    */
 }
